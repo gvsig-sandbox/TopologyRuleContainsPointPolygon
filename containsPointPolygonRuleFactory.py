ec42bd60776cc671c9d6a81818810fd634e7b89e
Mauro Carlevaro <mauroctecno@gmail.com>
2019-07-25 08:41:47 -0300

    structure creation

---
+++ containsPointPolygonRuleFactory.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+
+import gvsig
+import sys
+
+from gvsig import uselib
+uselib.use_plugin("org.gvsig.topology.app.mainplugin")
+
+from org.gvsig.fmap.geom import Geometry
+from org.gvsig.tools.util import ListBuilder
+from org.gvsig.topology.lib.api import TopologyLocator
+from org.gvsig.topology.lib.spi import AbstractTopologyRuleFactory
+
+from containsPointPolygonRule import ContainsPointPolygonRule
+
+class ContainsPointPolygonRuleFactory(AbstractTopologyRuleFactory):
+      
+    def __init__(self):
+        AbstractTopologyRuleFactory.__init__(
+            self,
+            "ContainsPointPolygon",
+            "contains Point Polygon",
+            "This rule requires that",
+            ListBuilder().add(Geometry.TYPES.POINT).add(Geometry.TYPES.MULTIPOINT).asList(),
+            ListBuilder().add(Geometry.TYPES.SURFACE).add(Geometry.TYPES.MULTISURFACE).asList()
+        )
+    
+    def createRule(self, plan, dataSet1, dataSet2, tolerance):
+        rule = ContainsPointPolygonRule(plan, self, tolerance, dataSet1, dataSet2)
+        return rule
+
+def selfRegister():
+    try:
+        manager = TopologyLocator.getTopologyManager()
+        manager.addRuleFactories(ContainsPointPolygonRuleFactory())
+    except:
+        ex = sys.exc_info()[1]
+        gvsig.logger("Can't register rule. Class Name: " + ex.__class__.__name__ + ". Exception: " + str(ex), gvsig.LOGGER_ERROR)
+
+def main(*args):
+    pass
